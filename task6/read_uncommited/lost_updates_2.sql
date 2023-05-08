4411e5cd08d90cf9ba0969e0cdb1c826c42318c7
Daniil Medyakov <80580400+asapmed@users.noreply.github.com>
2023-05-08 21:41:49 +0300

    Add files via upload

---
+++ task6/read_uncommited/lost_updates_2.sql
@@ -0,0 +1,10 @@
+set transaction isolation level read uncommitted
+begin transaction
+
+
+update person_general_information set date_of_birth=dateadd(day,3,date_of_birth)
+where person_id = 15
+
+commit
+select * from person_general_information
+where person_id = 15
